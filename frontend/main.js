e139e3bf6e68466771ae3be8e9d89d1140fbaf0d
pdiddy95 <patrick.nolan@uq.net.au>
2019-08-23 20:09:58 +1000

    Started work on login page

--- frontend/main.js
+++ frontend/main.js
@@ -1,1 +1,6 @@
-console.log("hello world");
+const button = document.getElementById("login");
+const input = document.getElementById("username");
+
+button.addEventListener("click", function(event) {
+    console.log(input.value);
+})
