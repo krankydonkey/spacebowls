f3583bb3466a053f943ed972bc7768b88341dcdc
pdiddy95 <patrick.nolan@uq.net.au>
2019-08-24 12:13:47 +1000

    transition from login to main page

--- frontend/main.js
+++ frontend/main.js
@@ -3,4 +3,16 @@ const input = document.getElementById("username");
 
 button.addEventListener("click", function(event) {
     console.log(input.value);
+    // put code here
+    goToMain();
 })
+
+function goToLogin() {
+    document.getElementById("main-page").style.display = "none";
+    document.getElementById("login-page").style.display = "flex";
+}
+
+function goToMain() {
+    document.getElementById("login-page").style.display = "none";
+    document.getElementById("main-page").style.display = "flex";
+}
