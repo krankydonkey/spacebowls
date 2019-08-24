91f88712e5dabd9e518651186a009539ff714065
sannibunny <sanni.b123@gmail.com>
2019-08-24 13:49:07 +1000

    get_players

--- backend/spacebowls.py
+++ backend/spacebowls.py
@@ -12,6 +12,7 @@ list_of_players = []
 app.static("/static", "../frontend/dist")
 app.static("/", "../frontend/dist/index.html")
 
+
 @app.route("/test_players", methods=["POST"])
 async def get_name(request):
     """
@@ -29,13 +30,31 @@ async def get_name(request):
     player_name = request.json["name"]
 
     if len(list_of_players) > 7:
-        return json("Too many players, try later")
+        return json("Too many players, try later.")
     elif player_name not in list_of_players:
         list_of_players.append(player_name)
     else:
-        return json("ERROR")
+        return json("Please enter another name.")
 
     return json({"players": list_of_players})
+
+
+@app.route("/", methods=["POST"])
+async def get_players(request):
+    """
+    Returns the list of players currently in the game.
+
+    Parameter:
+        request (Request): The request made by the client, which includes the
+         name of the player.
+
+    Returns:
+        (Request): A request object in json format which includes the entire
+        list of players.
+    """
+    return json({"players": list_of_players})
+
+# TODO: clear array method
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8000)
